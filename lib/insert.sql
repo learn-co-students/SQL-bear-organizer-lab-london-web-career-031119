974457bb3b3c2a1f8a1618741153fb24162ebb00
mrsalvini <dsgmcosta@gmail.com>
2019-03-18 17:00:02 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name) VALUES ("Mr. Chocolate"), ("Rowdy"), ("Tabitha"), ("Sergeant Brown"), ("Melissa"), ("Grinch"), ("Wendy"), (NULL);
+
+-- Mr. Chocolate
+-- Rowdy
+-- Tabitha
+-- Sergeant Brown
+-- Melissa
+-- Grinch
+-- Wendy
+-- unnamed (the bear that killed Tim didn't have a name; refer back to how to create a record that doesn't have one value)
