5909c816f921869bc00a1a329d99e8c7248b3769
keoki33 <gwong323@yahoo.com>
2019-03-20 23:24:43 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO bears (name, age, temperament, color, alive) VALUES ('Mr. Chocolate', 3, 'happy', "brown", 1);
+INSERT INTO bears (name, age, temperament, color, alive) VALUES ('Rowdy', 3, 'sad', "brown", 0);
+INSERT INTO bears (name, age, temperament, color, alive) VALUES ('Tabitha', 3, 'happy', "brown", 1);
+INSERT INTO bears (name, age, temperament, color, alive) VALUES ('Sergeant Brown', 3, 'happy', "brown", 1);
+INSERT INTO bears (name, age, temperament, color, alive) VALUES ('Melissa', 3, 'happy', "brown", 1);
+INSERT INTO bears (name, age, temperament, color, alive) VALUES ('Grinch', 3, 'happy', "brown", 1);
+INSERT INTO bears (name, age, temperament, color, alive) VALUES ('Wendy', 3, 'happy', "brown", 1);
+INSERT INTO bears (name, age, temperament, color, alive) VALUES (NULL, 3, 'happy', "brown", 1);
